80466b6ce6ace3e18e57f52738e4b1160adf5307
Devone Austin <devone.austin@gmail.com>
2020-04-15 01:29:46 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,7 +7,8 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-
+  ["Black", "White", "Red", "Green"]
+  << "Yellow"
 end
 
 def add_element_to_start_of_array(array, element)
