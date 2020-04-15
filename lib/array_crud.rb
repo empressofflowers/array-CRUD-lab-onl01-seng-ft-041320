4f2576f1c825e14b9e2fad5a840a079e0bbf19b1
Devone Austin <devone.austin@gmail.com>
2020-04-15 02:56:50 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,7 +3,7 @@ def create_an_empty_array
 end
 
 def create_an_array
-  the_four_elemnts = ["Earth", "Air", "Water", "Fire"]
+  the_four_elements = ["Earth", "Air", "Water", "Fire"]
 end
 
 def add_element_to_end_of_array(array, element)
