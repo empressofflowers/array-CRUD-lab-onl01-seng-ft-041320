1cc60355bfdf842e340e6f00774d7ae7df611dbf
Devone Austin <devone.austin@gmail.com>
2020-04-15 02:24:38 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,8 +7,9 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  four_elements = ["Earth", "Air", "Water", "Fire"
+  four_elements = ["Earth", "Air", "Water", "Fire"]
   four_elements << "Purple"
+    puts four_elements.inspect
 end
 
 def add_element_to_start_of_array(array, element)
